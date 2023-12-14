cde04b4f17e3e9b15e24fada3788f34e16b19e04
JR <jadedResearcher@gmail.com>
2023-12-13 23:03:23 -0500

    misses work

--- InterloperSim/interloper.js
+++ InterloperSim/interloper.js
@@ -56,9 +56,11 @@ const mapping = {
   217: { video: "rewards/gods_unlock_apocalypse.mp4", images: ["rewards/apocalypse_1.PNG", "rewards/apocalypse_2.PNG", "rewards/apocalypse_3.PNG"] }
 }
 
+const blankImage = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
 
 
-const handleID = (id) => {
+
+const handleID = async (id) => {
   const rand = new SeededRandom(id);
   const room = document.querySelector("#room-container");
   //475 by 383 internals
@@ -69,34 +71,78 @@ const handleID = (id) => {
       const video = createElementWithClassAndParent("video", room);
       video.src = hit.video;
       video.height = 475;
+      video.style.cursor="pointer";
       video.width = 475;
       video.autoplay = true;
+      video.controls = false;
       window.onclick = () => {
         video.play();
 
       }
-      video.controls = true;
       video.loop = true;
-      fuckShitUP(room, hit.images, 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=', rand);
+      //fuckShitUP(100,room, hit.images,blankImage , rand);
     }
   } else {
-    window.alert("miss");
+    await grabEyesImages();
+    let images = [rand.pickFrom(eyes1)];
+    if(rand.nextDouble()>0.5){
+      images.push(rand.pickFrom(eyes2));
+    }
+
+    if(rand.nextDouble()>0.5){
+      images.push(rand.pickFrom(eyes3));
+    }
+
+    if(rand.nextDouble()>0.5){
+      images.push(rand.pickFrom(eyes4));
+    }
+
     /*
       if the number is a multiple of any known key, glitch it minimally
       and even work in one of the images from it with the other bits
     */
 
+    fuckShitUP(rand.getRandomNumberBetween(10,100),room, images,images[0] , rand);
+
+
   }
+
+}
+
+let eyes1 = [];
+let eyes2 = [];
+let eyes3 = [];
+let eyes4 = [];
+
+
+const grabEyesImages = async () => {
+  let image_root = "http://farragofiction.com/ZampanioEyes/"
+  let tmp = await getImages(image_root);
+  eyes1 = tmp.map((item) => image_root + item);
+
+  image_root = "http://farragofiction.com/ZampanioEyes2/"
+  tmp = await getImages(image_root);
+  eyes2 = tmp.map((item) => image_root + item);
+
+  image_root = "http://farragofiction.com/ZampanioEyes3/"
+  tmp = await getImages(image_root);
+  eyes3 = tmp.map((item) => image_root + item);
+
+  image_root = "http://farragofiction.com/ZampanioEyes4/"
+  tmp = await getImages(image_root);
+  eyes4 = tmp.map((item) => image_root + item);
+
 
 }
 
 
 //modified from jackElope/lavinraca/lavinraca
-fuckShitUP = async (wrapper, images, secret_image, rand) => {
+//fuckery_quotient smaller is more fucky
+fuckShitUP = async (fuckery_quotient, wrapper, images, secret_image, rand) => {
   let fucked_up_image_holder = createElementWithClassAndParent("div", wrapper, "holder");
 
   fucked_up_image_holder.style.background = "black";
-  const size = 100;
+  const size = fuckery_quotient;
 
   for (let y = 0; y < 475; y += size) {
     for (let x = 0; x < 475; x += size) {
@@ -105,21 +151,22 @@ fuckShitUP = async (wrapper, images, secret_image, rand) => {
       box.style.position = "absolute";
       box.style.top = y + "px";
       box.style.left = x + "px";
-      box.style.backgroundPositionY = 200 - y + "px";
-      box.style.backgroundPositionX = 200 - x + "px";
+      box.style.backgroundPositionY = 475 - y + "px";
+      box.style.backgroundPositionX = 475 - x + "px";
       box.style.backgroundImage = `url('${rand.pickFrom(images)}')`;
       box.style.width = size + "px";
       box.style.height = size + "px";
       const odds = rand.nextDouble();
-      if (odds > 0.75) {
-        box.style.animation = `james-webb-telescope-mirrors-mirrored ${rand.nextDouble() * 4}s infinite linear 0s`
+      if (odds > 0.95) {
+        box.style.animation = `james-webb-telescope-mirrors-mirrored ${rand.nextDouble() * fuckery_quotient}s infinite linear 0s`
       } else if (odds > 0.5) {
-        box.style.animation = `james-webb-telescope-mirrors-mirrored  ${rand.nextDouble() * 4}s infinite linear 2s`
+        box.style.animation = `james-webb-telescope-mirrors-mirrored  ${rand.nextDouble() * fuckery_quotient}s infinite linear 2s`
         box.style.backgroundImage = `url('${secret_image}')`;
       } else {
-        box.style.animation = `james-webb-telescope-mirrors-mirrored  ${rand.nextDouble() * 4}s infinite linear 1s`
+        box.style.animation = `james-webb-telescope-mirrors-mirrored  ${rand.nextDouble() * fuckery_quotient}s infinite linear 1s`
       }
     }
 
   }
 }
+
