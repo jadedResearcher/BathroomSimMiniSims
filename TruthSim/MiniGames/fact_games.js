8c5893d8e29d5f51de2064b9262728543fda92cf
Jennifer Reynolds <jadedResearcher@gmail.com>
2024-05-26 23:12:08 -0400

    neville and devona

--- TruthSim/MiniGames/fact_games.js
+++ TruthSim/MiniGames/fact_games.js
@@ -9,17 +9,55 @@ class TwinsMiniGame extends MiniGame {
         super(TWINSMINIEGAME);
     }
 
-    startGame = (ele, room, callback) => {
-        window.alert("JR NOTE: TODO");
-        this.valuableCustomer(ele, callback);
+    singleFactInfoDump = (fact, ele, room, callback) => {
+        const devonalabel = createElementWithClassAndParent("div", ele);
+        //she adds all the emphasis she can to the point its hard to tell whats going on
+        devonalabel.innerHTML = `<hr><u><img height="31" src='images/devona_twin_by_guide.png'>Devona says: 'Here's Everything I Know About <i><b>'${fact.title}'</b></i> I Hope It's Okay!'</u><br><br>` //because she is too much information all at once not using css for her, she is all in the html, a mess of content and design that is practically unreadable
+
+        const infodump = createElementWithClassAndParent("div", ele);
+        infodump.style.padding = '31px';
+        infodump.style.border = "1px dashed yellow";
+        infodump.style.marginLeft = "auto";
+        infodump.style.marginRight = "auto";
+        infodump.style.width = "50%";
+        infodump.style.marginBottom = "13px";
+        infodump.innerHTML = fact.lore_snippet;
+
+        const buttonLabel = createElementWithClassAndParent("div", ele, "void neville-button"); //while neville is entirely css, even the text, don't worry you don't need to see anything :) :) :)
+
+        const button = createElementWithClassAndParent("button", buttonLabel); //while neville is entirely css, even the text, don't worry you don't need to see anything :) :) :)
+        button.innerText= "Forget?"
+        button.onclick = async () => {
+            globalDataObject.factsUnlocked = removeItemOnce(globalDataObject.factsUnlocked, fact);
+
+            if(fact === this.fact){
+                this.fact = undefined; //forget about it
+            }
+            this.startGame(ele, room, callback);
+        }
     }
 
-    
+    startGame = (ele, room, callback) => {
+        ele.innerHTML="";
+        if(this.fact){
+            this.singleFactInfoDump(this.fact, ele, room, callback);
+        }else{
+
+            for(let fact of globalDataObject.factsUnlocked){
+                this.singleFactInfoDump(fact, ele, room, callback);
+            }
+        }
+
+        this.valuableCustomer(ele, callback);
+
+    }
+
+
     valuableCustomer = (ele, callback) => {
         const button = createElementWithClassAndParent("button", ele);
-        button.innerText = "JR NOTE: TODO REAL GAME BUT FOR NOW CLICK THIS TO ESCAPE";
+        button.innerText = "The Twins Are Just Happy To InfoDump, You're Free To Leave!";
         button.onclick = async () => {
-            await truthPopup("You are a real gamer!", "Wow! It seems you are having so much fun earning points and leveling up by participating in capitalism!", "While I value the Chief Financial Officer of Eyedol Game's ability to draw people in, my hot maze gf is much, much better.");
+            await truthPopup("You learned about facts!", "Was it fun? It looked fun!", "Yes. It seems this is actually good. Learning more information about the various spirallling plots of Zampanio seems to draw many people in. Continue. Additionally, I appreciate the female twin, Devona's, attempt to map me.");
             callback(globalDataObject.currentMaze);
             renderMazeTab();
         }
@@ -29,7 +67,9 @@ class TwinsMiniGame extends MiniGame {
     render = (ele, room, callback) => {
 
         this.initializeRender(ele);
-        const container = this.setupGameHeader(ele, room, callback, "The Twins, Neville and Devona will allow you to Destroy Irrelevancy and Ignorance! ", undefined, "images/twins_by_guide.png")
+        const title = this.fact ? "Fact Chosen: " + this.fact.title : "Wait! Wait!!! What Fact Did You Want To Know About Oh God I Don't Know What To Do, Neville, Maybe I Should Just Pick All The Facts?";
+
+        const container = this.setupGameHeader(ele, room, callback, "The Twins, Neville and Devona will allow you to Destroy Irrelevancy and Ignorance! " + title, undefined, "images/twins_by_guide.png")
 
     }
 }
