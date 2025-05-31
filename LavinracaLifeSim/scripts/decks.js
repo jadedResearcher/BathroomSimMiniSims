601b99a194b59b924b5381a1d9957ad46fcbe143
Jennifer Reynolds <jadedResearcher@gmail.com>
2025-05-31 11:23:40 -0400

    can at least skip looping

--- LavinracaLifeSim/scripts/decks.js
+++ LavinracaLifeSim/scripts/decks.js
@@ -55,3 +55,35 @@ class CardSet {
 }
 
 const genericCardset = new CardSet();
+
+
+
+/*
+ i need you to understand that i am adopting *kittens*
+
+ itty bitty kittens
+
+ they're too young to take home yet
+ 
+ but the shelters letting me visit them tomorrow and i am so hype i can't codes
+
+ im gonna turn their pics and videos into so many spooky things
+ 
+*/
+
+/*
+okay actually no, that didn't work out, the kittens were too sick to visit AND someone else adopted them
+
+but i found BETTER than them. 
+
+(sorry jimbo, i know your name was perfect but it wasn't meant to be)
+
+i found an eyeless kitten named hallow, claimed to be born on halloween and his best friend who breathes really loud and scary but that lets him follow her around
+
+they're so freaking adorable and loving and cute and hallow the eyesless void is so completely my aesthetic
+
+faceless little kitty, its like the slugcatsona the herald made for me
+*/
+
+//https://www.tumblr.com/jadedresearcher/783279833598803968/eve-is-my-other-new-kitten-and-she-is-a-seeing?source=share
+//https://www.tumblr.com/jadedresearcher/783279651810377728/everyone-stop-everything-theyre-doing-and-look-at?source=share
